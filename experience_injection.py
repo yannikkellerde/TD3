c479826c6e3fb2999728543f4b8a720bf5e0b0ea
yannikkellerde <yannik@kelnet.de>
2021-01-17 01:14:25 +0100

    timesteppunish

--- experience_injection.py
+++ experience_injection.py
@@ -12,11 +12,13 @@ parser.add_argument("--env", default="water_pouring:Pouring-mdp-full-v0")       
 parser.add_argument("--replay_buffer_path",type=str,default="replay_buffers")
 parser.add_argument("--policy_path",type=str)
 parser.add_argument("--noise",type=float,default=0.3)
+parser.add_argument("--time_step_punish",type=float,default=0.1)
 parser.add_argument("--num_inject",type=int,default=1e5)
 args = parser.parse_args()
 
 rtpt = RTPT(name_initials='YK', experiment_name="initialization", max_iterations=int(args.num_inject))
 env = gym.make(args.env,use_gui=False)
+env.time_step_punish = args.time_step_punish
 replay_buffer = ReplayBuffer(env.observation_space,env.action_space)#,load_folder=args.replay_buffer_path)
 print("loaded_replay_buffer")
 policy = np.load(args.policy_path)
